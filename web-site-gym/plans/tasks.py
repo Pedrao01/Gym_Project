999bcb36cd6f8a240e3e00a208b8ae1d41aa2634
Pedrao01 <149744853+Pedrao01@users.noreply.github.com>
2026-07-02 11:50:04 -0300

    feat(plans): Async tasks to plan

---
+++ web-site-gym/plans/tasks.py
@@ -0,0 +1,16 @@
+from celery import shared_task
+from .models import Plan
+from django.utils import timezone
+
+
+@shared_task
+def check_expired_plans():
+    today = timezone.now().date()
+    expired_plans = Plan.objects.filter(
+        is_valid=True,
+        expected_payment__isnull=False,
+        expected_payment__lte=today
+
+    )
+    total = expired_plans.update(is_active=False, is_valid=False, expected_payment=None)
+    print(f'{total} expired plans updated')
