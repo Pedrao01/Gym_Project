127d4196faba2a0be42c017a7c1b9434e6f7a482
Pedrao01 <149744853+Pedrao01@users.noreply.github.com>
2026-07-02 11:46:29 -0300

    feat(plans): Types of plans

---
+++ web-site-gym/plans/plans.py
@@ -0,0 +1,20 @@
+PLANS = {
+    'mensal': {
+        'title': 'Plano Mensal',
+        'unit_price': 1.00,
+        'description': 'Acesso completo à academia · Sem fidelidade',
+        'number_months': 1
+    },
+    'trimestral': {
+        'title': 'Plano Trimestral',
+        'unit_price': 1.00,
+        'description': 'Acesso completo à academia · Economia de R$48',
+        'number_months': 3
+    },
+    'anual': {
+        'title': 'Plano Anual',
+        'unit_price': 1.00,
+        'description': 'Acesso completo à academia · Economia de R$289',
+        'number_months': 12
+    },
+}
