d76d4e66a182ea30b45d432b78c2f6cd9b9b55a1
Pedrao01 <149744853+Pedrao01@users.noreply.github.com>
2026-07-19 00:10:53 -0300

    feat(user): serializer about userAdmin(stats)

--- web-site-gym/users/serializers.py
+++ web-site-gym/users/serializers.py
@@ -1,14 +1,16 @@
 from rest_framework import serializers
 from .models import User
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer, AuthUser, Token
+from plans.serializers import PlanSerializer
 
 
 class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(min_length=8, write_only=True)
+    plan = PlanSerializer(read_only=True)
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'phone_number', 'password']
+        fields = ['id', 'username', 'email', 'phone_number', 'password', 'plan']
         extra_kwargs = {
             'is_staff': {'read_only': True}
         }
@@ -29,3 +31,9 @@ class MyTokenSerializer(TokenObtainPairSerializer):
         token['is_staff'] = user.is_staff
 
         return token
+
+
+class StatsSerializer(serializers.ModelSerializer):
+    total = serializers.IntegerField(read_only=True)
+    with_plan = serializers.IntegerField(read_only=True)
+    without_plan = serializers.IntegerField(read_only=True)
