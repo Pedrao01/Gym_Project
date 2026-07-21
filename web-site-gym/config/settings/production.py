636506035bbf4688547110a421b70822fa973d50
Pedrao01 <149744853+Pedrao01@users.noreply.github.com>
2026-07-21 11:55:16 -0300

    refactor(config): adds environment variables with decouple

--- web-site-gym/config/settings/production.py
+++ web-site-gym/config/settings/production.py
@@ -1,9 +1,10 @@
 from .base import *
+from decouple import config
 
 DEBUG = False
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 CORS_ALLOW_ALL_ORIGINS = False
-# CORS_ALLOWED_ORIGINS = ["https://seu-frontend.vercel.app"]  # Production
+CORS_ALLOWED_ORIGINS = config('CORS_ALLOWED_ORIGINS', default='http://localhost:5173', cast=Csv())  # Production
 
 SECURE_SSL_REDIRECT = True
 SESSION_COOKIE_SECURE = True
