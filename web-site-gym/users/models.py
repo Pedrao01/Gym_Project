4c65f687fa3ca11f8093138d17874a90076f3333
Pedrao01 <149744853+Pedrao01@users.noreply.github.com>
2026-08-01 10:46:48 -0300

    fix(users):declared phone_number field from user model like unique

--- web-site-gym/users/models.py
+++ web-site-gym/users/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     email = models.EmailField(unique=True, max_length=254)
-    phone_number = models.CharField(max_length=11, blank=False)
+    phone_number = models.CharField(max_length=11, blank=False, unique=True)
 
     def __str__(self):
         return f'Username: {self.username} - Email: {self.email}'
