0792eed72a42b1979202f729a9f17295a3ef473e
Pedrao01 <149744853+Pedrao01@users.noreply.github.com>
2026-08-21 10:24:01 -0300

    refactor: The plan prices have been changed

--- web-site-gym/plans/plans.py
+++ web-site-gym/plans/plans.py
@@ -1,19 +1,19 @@
 PLANS = {
     'mensal': {
         'title': 'Plano Mensal',
-        'unit_price': 1.00,
+        'unit_price': 50.00,
         'description': 'Acesso completo à academia · Sem fidelidade',
         'number_months': 1
     },
     'trimestral': {
         'title': 'Plano Trimestral',
-        'unit_price': 1.00,
+        'unit_price': 120.00,
         'description': 'Acesso completo à academia · Economia de R$48',
         'number_months': 3
     },
     'anual': {
         'title': 'Plano Anual',
-        'unit_price': 1.00,
+        'unit_price': 550,
         'description': 'Acesso completo à academia · Economia de R$289',
         'number_months': 12
     },
