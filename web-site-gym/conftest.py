51e3b29396d4bc4fd565f07009af5c4daaa27b8a
Pedrao01 <149744853+Pedrao01@users.noreply.github.com>
2026-08-22 08:26:22 -0300

    test: global fixtures configuration

---
+++ web-site-gym/conftest.py
@@ -0,0 +1,45 @@
+import pytest
+from rest_framework.test import APIClient
+from plans.models import Plan
+from django.utils import timezone
+from dateutil.relativedelta import relativedelta
+
+
+@pytest.fixture
+def api_client():
+    return APIClient()
+
+
+@pytest.fixture
+def authenticated_client(api_client, valid_user):
+    api_client.force_authenticate(user=valid_user)
+    return api_client
+
+
+@pytest.fixture
+def valid_user(db, django_user_model):
+    return django_user_model.objects.create_user(
+        username='pedro',
+        email='pedro@gmail.com',
+        phone_number='74999668392',
+        password='pedro123'
+    )
+
+
+@pytest.fixture
+def valid_plan(db, valid_user):
+    return Plan.objects.create(
+        user=valid_user,
+        kind_plan='mensal',
+        expected_payment=timezone.now().date() + relativedelta(months=1)
+    )
+
+
+@pytest.fixture
+def valid_user_1(db, django_user_model):
+    return django_user_model.objects.create_user(
+        username='manel',
+        email='manel@gmail.com',
+        phone_number='74999873645',
+        password='manel123'
+    )
