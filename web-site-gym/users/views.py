bbbed44bc70b0e8758c1fa90c312c89d92d19b84
Pedrao01 <149744853+Pedrao01@users.noreply.github.com>
2026-08-30 13:28:46 -0300

    refactor: comparison removed in the UserView

--- web-site-gym/users/views.py
+++ web-site-gym/users/views.py
@@ -59,16 +59,8 @@ class UserView(APIView):
         )
 
         if serializer.is_valid():
-            result = update_user(user, **serializer.validated_data)
-
-            if result > 0:
-                return Response({
-                    'result': result,
-                }, status=status.HTTP_200_OK)
-
-            return Response({
-                'error': 'error at try update user'
-                }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+            update_user(user, **serializer.validated_data)
+            return Response({}, status=status.HTTP_200_OK)
 
         return Response({}, status=status.HTTP_400_BAD_REQUEST)
 
