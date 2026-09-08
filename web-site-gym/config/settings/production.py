b1ddc5d806d07c68f4792361c8a8120b6dfbe88f
Pedrao01 <149744853+Pedrao01@users.noreply.github.com>
2026-09-08 12:41:40 -0300

    refactor: cors_allowed_origins varible

--- web-site-gym/config/settings/production.py
+++ web-site-gym/config/settings/production.py
@@ -4,7 +4,7 @@ from decouple import config
 DEBUG = False
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 CORS_ALLOW_ALL_ORIGINS = False
-CORS_ALLOWED_ORIGINS = config('CORS_ALLOWED_ORIGINS', default='http://localhost:5173', cast=Csv())  # Production
+CORS_ALLOWED_ORIGINS = config('CORS_ALLOWED_ORIGINS', cast=Csv())  # Production
 
 SECURE_SSL_REDIRECT = True
 SESSION_COOKIE_SECURE = True
