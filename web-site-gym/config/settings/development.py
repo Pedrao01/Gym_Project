b8df89624c9aa0a9319bbd6a723edc37f5af1c31
Pedrao01 <149744853+Pedrao01@users.noreply.github.com>
2026-09-25 20:50:44 -0300

    feat: tell to flake8 that this is intentional

--- web-site-gym/config/settings/development.py
+++ web-site-gym/config/settings/development.py
@@ -1,4 +1,4 @@
-from .base import *
+from .base import *  # noqa: F401, F403
 
 DEBUG = True
 CELERY_WORKER_POOL = 'solo'
--- web-site-gym/config/settings/production.py
+++ web-site-gym/config/settings/production.py
@@ -1,4 +1,4 @@
-from .base import *
+from .base import *  # noqa: F401, 403
 from decouple import config
 
 DEBUG = False
